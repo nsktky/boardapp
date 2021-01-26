a150e9e25ee8d836f90fb1644981451725f74162
nsktky <nosaka.takuya11@gmail.com>
2021-01-26 22:33:46 +0900

    ユーザー登録の重複処理

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 # djangoが提供しているUserModelを使用
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 def signupfunc(request):
     # レスポンスがpostだった場合（signup.htmlのform入力があった場合）の動作
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        # create_userでユーザーを作成。引数にユーザー名、メールアドレス、パスワードを渡す
-        user = User.objects.create_user(username, '', password)
+        # ユーザー作成時、重複していたらexcept
+        try:
+            # create_userでユーザーを作成。引数にユーザー名、メールアドレス、パスワードを渡す
+            user = User.objects.create_user(username, '', password)
+            return render(request, 'signup.html', {'some': 100})
+        except IntegrityError:
+            return render(request, 'signup.html', {'error': 'このユーザーはすでに登録されています。'})
 
     # renderメソッドはhttpレスポンスを作成する
     return render(request, 'signup.html', {'some': 100})
