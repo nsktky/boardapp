d024ee7320f1879271e8c1b3d9d826d9acb10e37
nsktky <nosaka.takuya11@gmail.com>
2021-01-26 23:01:16 +0900

    loginの実装

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # djangoが提供しているUserModelを使用
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+from django.contrib.auth import authenticate, login
 
+# サインイン関数。新規ユーザーの作成と重複登録を防ぐ処理をする
 def signupfunc(request):
     # レスポンスがpostだった場合（signup.htmlのform入力があった場合）の動作
     if request.method == 'POST':
@@ -12,9 +14,26 @@ def signupfunc(request):
         try:
             # create_userでユーザーを作成。引数にユーザー名、メールアドレス、パスワードを渡す
             user = User.objects.create_user(username, '', password)
+            # renderメソッドはhttpレスポンスを作成する
             return render(request, 'signup.html', {'some': 100})
         except IntegrityError:
             return render(request, 'signup.html', {'error': 'このユーザーはすでに登録されています。'})
 
-    # renderメソッドはhttpレスポンスを作成する
-    return render(request, 'signup.html', {'some': 100})
+    return render(request, 'signup.html')
+
+
+# ログイン関数。登録されたユーザーを認証する。
+def loginfunc(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return render(request, 'login.html', {'context':'logged in'})            
+        else:
+            return render(request, 'login.html', {'context':'not logged in'})
+    return render(request, 'login.html', {'context':'get method'})            
+
+
